63d732d88ce35c6e3b659b94f0350fd07ebfd6be
Nidhi Singhal <82880062+nidhiss29@users.noreply.github.com>
2022-09-08 23:27:54 +0530

    Add files via upload

---
+++ subofprodandsum.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int subtractProductAndSum(int n)
+    {
+
+        int result, len;
+        int dig, prod = 1, sum = 0;
+        vector<int> arr;
+
+        while (n != 0)
+        {
+            dig = n % 10;
+            n = n / 10;
+            arr.push_back(dig);
+        }
+
+        len = arr.size();
+        for (int i = 0; i < len; i++)
+        {
+            prod *= arr[i];
+            sum += arr[i];
+        }
+        result = prod - sum;
+        return result;
+    }
+};
+
+/* in while loop only you can apply prod ans um command so a compact code prod*=dig side by side*/
