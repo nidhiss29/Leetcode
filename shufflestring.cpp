1ee5af6ffbd2846f111816c081a9f9b43acddac4
Nidhi Singhal <82880062+nidhiss29@users.noreply.github.com>
2022-09-10 17:42:33 +0530

    Add files via upload

---
+++ shufflestring.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    string restoreString(string s, vector<int> &indices)
+    {
+        string final = "";
+        int len = indices.size();
+        vector<char> strhi(len, 0);
+        for (int i = 0; i < len; i++)
+        {
+            strhi[indices[i]] = s[i];
+        }
+        for (int i = 0; i < strhi.size(); i++)
+        {
+            final += strhi[i];
+        }
+        return final;
+    }
+};
