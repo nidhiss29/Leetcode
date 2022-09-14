44701592d931b89778abd76e0c052fafe6837991
Nidhi Singhal <82880062+nidhiss29@users.noreply.github.com>
2022-09-14 21:56:42 +0530

    Update 2006.cpp

--- 2006.cpp
+++ 2006.cpp
@@ -18,4 +18,20 @@ int countKDifference(vector<int>& nums, int k) {
     return res;
 }
 
+
+
+
+
+abs(nums[i]-nums[j])=k;
+if(nums[i]-nums[j]<0) then nums[j]>nums[i];
+
+if(nums[i]-nums[j]>0) then nums[i]>nums[j];
+
+sorting will make sure that 2 condition will never happen so we only have to concern about the 1 condition;
+
+nums[j]>nums[i];
+which mean we can write the formula in question like:
+nums[j]-nums[i]=k;
+nums[j]=nums[i]+k;
+
 */
