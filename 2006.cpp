bba3794ce2c361b89912f935e02abdd5777dcb5b
Nidhi Singhal <82880062+nidhiss29@users.noreply.github.com>
2022-09-14 21:55:03 +0530

    Add files via upload

---
+++ 2006.cpp
@@ -0,0 +1,21 @@
+int countKDifference(vector<int> &nums, int k)
+{
+    int cnt[101] = {}, res = 0;
+    for (auto n : nums)
+        ++cnt[n];
+    for (int i = k + 1; i < 101; ++i)
+        res += cnt[i] * cnt[i - k];
+    return res;
+}
+
+/* OPTIMIZED
+int countKDifference(vector<int>& nums, int k) {
+    int cnt[101] = {}, res = 0;
+    for (auto n : nums)
+        ++cnt[n];
+    for (int i = k + 1; i < 101; ++i)
+        res += cnt[i] * cnt[i - k];
+    return res;
+}
+
+*/
